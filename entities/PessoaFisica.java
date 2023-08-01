f8d711da817a2376d9cd2154e9bda2bd20ba6af6
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-08-01 20:47:34 -0300

    Create PessoaFisica.java

---
+++ entities/PessoaFisica.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class PessoaFisica extends Pessoa {
+	private Double gastosSaude;
+	
+	public PessoaFisica() {
+		
+	}
+
+	public PessoaFisica(String name, Double renda, Double gastosSaude) {
+		super(name, renda);
+		this.gastosSaude = gastosSaude;
+	}
+
+	public Double getGastosSaude() {
+		return gastosSaude;
+	}
+
+	public void setGastosSaude(Double gastosSaude) {
+		this.gastosSaude = gastosSaude;
+	}
+
+	@Override
+	public final Double rendaAnual() {
+		if(renda <= 2000.00) {
+			return (renda*0.15)-(gastosSaude*0.15);
+		}
+		else {
+			return (renda*0.25)-(gastosSaude*0.5);
+		}	
+	}
+
+	@Override
+	public String toString() {
+		return name + ": $ " + String.format("%.2f", rendaAnual()) + "\n";
+	}	
+}
