a8868c02f12f45bf860412e163250df6b86b67f7
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-08-01 20:47:54 -0300

    Create PessoaJuridica.java

---
+++ entities/PessoaJuridica.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class PessoaJuridica extends Pessoa{
+	private Integer funcionarios;
+	
+	public PessoaJuridica() {
+		
+	}
+
+	public PessoaJuridica(String name, Double renda, Integer funcionarios) {
+		super(name, renda);
+		this.funcionarios = funcionarios;
+	}
+
+	public Integer getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(Integer funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	@Override
+	public Double rendaAnual() {
+		if(funcionarios >= 10) {
+			return renda*0.14;
+		}
+		else {
+			return renda*0.16;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return name + ": $ " + String.format("%.2f", rendaAnual()) + "\n";
+	}	
+}
