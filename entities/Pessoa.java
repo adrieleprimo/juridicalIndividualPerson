325899c5fbee07f93f4398c0d856e626a7ba233f
Adriele <83988106+adrieleprimo@users.noreply.github.com>
2023-08-01 20:47:08 -0300

    Create Pessoa.java

---
+++ entities/Pessoa.java
@@ -0,0 +1,33 @@
+package entities;
+
+public abstract class Pessoa {
+	protected String name;
+	protected Double renda;
+	
+	public Pessoa() {
+		
+	}
+
+	public Pessoa(String name, Double renda) {
+		this.name = name;
+		this.renda = renda;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getRenda() {
+		return renda;
+	}
+
+	public void setRenda(Double renda) {
+		this.renda = renda;
+	}
+	
+	public abstract Double rendaAnual();
+}
